bc254d2e11ebca9a4831f463412dad3e75155497
Ansh Kumar <anshkumar8710@gmail.com>
2025-11-13 18:44:09 +0530

    added singleton pattern for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "../app/generated/prisma/client";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient());
+
+export default prisma;

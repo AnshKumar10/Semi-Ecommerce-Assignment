6c39ce38020af8b3b4c043a3932756b638922c98
Ansh Kumar <anshkumar8710@gmail.com>
2025-11-14 08:27:28 +0530

    Added prisma as dep

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../prisma/generated/client/client";
+import { PrismaClient } from "../app/generated/client/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as {
